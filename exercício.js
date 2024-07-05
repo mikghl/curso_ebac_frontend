103db869bf5ae8a423788bb02bcb46654fa8adab
Micael_S.R <137561767+mikghl@users.noreply.github.com>
2024-07-05 15:37:30 -0300

    Add files via upload

---
+++ exercício.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+console.log(multiplicacao(6, 6));
+function saudacao(nome) {
+    return `Olá, ${nome}`;
+}
+;
+console.log(saudacao('Micael'));
---
+++ exercício.ts
@@ -0,0 +1,11 @@
+function multiplicacao(numero1: number, numero2: number): number {
+    return numero1 * numero2
+}
+
+console.log(multiplicacao(6,6));
+
+function saudacao(nome: string): string {
+    return `Olá, ${nome}`;
+};
+
+console.log(saudacao('Micael'));
